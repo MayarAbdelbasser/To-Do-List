ea3a7caac808dedc5d375307e2e6c38626524560
Mayar Abdelbasser <mayarabdelbasser539@gmail.com>
2024-04-16 22:56:33 +0200

    Modify js file

--- script.js
+++ script.js
@@ -1,21 +1,22 @@
 const task = document.querySelector("input");
 const section = document.querySelector("section");
 
-const tasksArray = ["First Task", "Second Task"];
+const tasksArray = [];
 console.log(tasksArray);
-
-document.querySelector(".first-task").textContent = tasksArray[0];
-document.querySelector(".second-task").textContent = tasksArray[1];
 
 document.querySelector("form").onsubmit = function (event) {
   event.preventDefault();
   const taskValue = task.value;
-  tasksArray.push(`${taskValue}`);
-  console.log(tasksArray);
+  if (taskValue.trim() == "") {
+    alert("There is no task to add");
+  } else {
+    tasksArray.push(`${taskValue.trim()}`);
+    console.log(tasksArray);
 
-  section.innerHTML += `<div><input type="checkbox" /><label>${
-    tasksArray[tasksArray.length - 1]
-  }</label></div>
-  `;
-  console.log(taskValue);
+    section.innerHTML += `<div><input type="checkbox" /><label>${
+      tasksArray[tasksArray.length - 1]
+    }</label></div>
+    `;
+    task.value = "";
+  }
 };
